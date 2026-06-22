162be749550faaf6a7755de2c4277a535fd058f6
rehan <rehan.shah70@yahoo.com>
2026-06-22 14:43:23 +0500

    ch 10 complete

---
+++ book_projects/lifetime/src/main.rs
@@ -0,0 +1,38 @@
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+//fn longestwithoutlifetime(x: &str, y: &str) -> &str {
+//    if x.len() > y.len() {
+//    x
+//  } else {
+//    y
+//}
+//}
+// using generic, trait and lifetime all together
+use std::fmt::Display;
+
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {ann}");
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn main() {
+    {
+        let string1 = String::from("long string is long");
+        let string2 = String::from("xyz");
+        let result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {result}");
+    }
+}
